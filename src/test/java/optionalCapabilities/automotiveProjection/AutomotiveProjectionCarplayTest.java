3bab874f50ded008803d9745cf78afee9edb6214
alavi <alavi@digital.ai>
2025-10-22 10:40:33 +0300

    add carplay test example

--- src/test/java/optionalCapabilities/automotiveProjection/AutomotiveProjectionCarplayTest.java
+++ src/test/java/optionalCapabilities/automotiveProjection/AutomotiveProjectionCarplayTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AutomotiveProjectionCarplayTest {
-
     static {
         nu.pattern.OpenCV.loadLocally(); // Load OpenCV native library
     }
